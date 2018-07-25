a3e7d494b99e9d76401d45830310925b9f65cb39
Nizamuddin <Nizamuddin@Nizamuddin-PC>
2018-07-26 01:29:18 +0530

    push to git

--- TestProjectForGit/src/testpack1/TestClass.java
+++ TestProjectForGit/src/testpack1/TestClass.java
@@ -5,6 +5,10 @@ public class TestClass {
   public static void main(String[] args)
   {
     System.out.println("Pulling latest changes from git to local");
+    
+    
+    System.out.println("pushing to git ");
+    
   }
   
 
